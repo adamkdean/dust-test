247736ab5a6f217f8e16890bab7e5cc49e5e82dd
Adam K Dean <adam@aspire-limited.co.uk>
2016-09-10 21:38:41 +0100

    example of broken context

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
 const path = require('path')
-const express = require('express')
 const hoffman = require('hoffman')
+const express = require('express')
 const request = require('request')
 const duster = require('duster')
 const dust = duster.dust
-const app = express()
 
-const baseViewModel = dust.context({
-  globalTest: true
+const app = express()
+const base = dust.context({
+  isBase: true,
+  age: 26,
+  people: [
+    { name: 'exampleA' },
+    { name: 'exampleB' }
+  ]
 })
 
-app.set('views', path.join(process.cwd(), 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'dust')
 app.engine('dust', hoffman.__express())
-app.use(hoffman.stream)
 
-app.get('/', (req, res) => {
-  const viewModel = baseViewModel.push({
-    collection: [
-      { name: 'first' },
-      { name: 'second' },
-      { name: 'third' }
-    ]
-  })
-  console.log('--------------------------')
-  console.log('viewModel', viewModel)
-  console.log('--------------------------')
-  res.render('index', { '__STACK__': viewModel })
+app.get('/', function (req, res) {
+  res.render('people', {})
 })
 
-app.listen(3100, () => console.log(`Listening on port 3100`))
+app.get('/pojo', function (req, res) {
+  res.render('people', {
+    age: 26,
+    people: [
+      { name: 'exampleA' },
+      { name: 'exampleB' }
+    ]
+  })
+})
 
+app.get('/context', function (req, res) {
+  res.render('people', base.push({
+    people: [
+      { name: 'adam' }
+    ]
+  }))
+})
 
-// app.get('/', function (req, res) {
-//   res.stream('hello', {
-//     'async': function(chunk, context, bodies, params) {
-//       return chunk.map(function(chunk) {
-//         // Introducting an artificial delay to make streaming more apparent
-//         setTimeout(function() {
-//           request('http://www.dustjs.com/')
-//           .on('data', chunk.write.bind(chunk))
-//           .on('end', chunk.end.bind(chunk));
-//         }, 3000);
-//       });
-//     }
-//   });
-// });
-
-// app.listen(3100, function () {
-//   console.log('Visit http://localhost:3100 to see streaming!');
-// });
+app.listen(4000, function () {
+  console.log('Visit http://localhost:4000 to see streaming!')
+})
