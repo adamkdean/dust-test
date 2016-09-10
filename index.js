1b7f38b3f7dfbfeb8060621a1df4490418834058
Adam K Dean <adam@aspire-limited.co.uk>
2016-09-10 21:24:55 +0100

    wip of hoffman example

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 const path = require('path')
 const express = require('express')
-const cons = require('consolidate')
-const dust = require('dustjs-linkedin')
-const helpers = require('dustjs-helpers')
-
-const workdir = process.cwd()
+const hoffman = require('hoffman')
+const request = require('request')
+const duster = require('duster')
+const dust = duster.dust
 const app = express()
 
 const baseViewModel = dust.context({
   globalTest: true
 })
 
-app.engine('dust', cons.dust)
-cons.dust.helpers = helpers
+app.set('views', path.join(process.cwd(), 'views'))
 app.set('view engine', 'dust')
-app.set('views', path.join(workdir, 'views'))
+app.engine('dust', hoffman.__express())
+app.use(hoffman.stream)
 
 app.get('/', (req, res) => {
   const viewModel = baseViewModel.push({
-    collection: [1, 2, 3]
+    collection: [
+      { name: 'first' },
+      { name: 'second' },
+      { name: 'third' }
+    ]
   })
-  res.render('index', viewModel)
+  console.log('--------------------------')
+  console.log('viewModel', viewModel)
+  console.log('--------------------------')
+  res.render('index', { '__STACK__': viewModel })
 })
 
 app.listen(3100, () => console.log(`Listening on port 3100`))
+
+
+// app.get('/', function (req, res) {
+//   res.stream('hello', {
+//     'async': function(chunk, context, bodies, params) {
+//       return chunk.map(function(chunk) {
+//         // Introducting an artificial delay to make streaming more apparent
+//         setTimeout(function() {
+//           request('http://www.dustjs.com/')
+//           .on('data', chunk.write.bind(chunk))
+//           .on('end', chunk.end.bind(chunk));
+//         }, 3000);
+//       });
+//     }
+//   });
+// });
+
+// app.listen(3100, function () {
+//   console.log('Visit http://localhost:3100 to see streaming!');
+// });
